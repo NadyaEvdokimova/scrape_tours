f9a94c2008aef8d504b681c802e17ac543243bf7
NadyaEvdokimova <146958813+NadyaEvdokimova@users.noreply.github.com>
2024-08-27 12:05:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,6 @@ def read(extracted_data):
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
     rows = cursor.fetchall()
-    print(rows)
     return rows
 
 
@@ -64,7 +63,6 @@ if __name__ == "__main__":
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
-        print(extracted)
         if extracted != "No upcoming tours":
             row = read(extracted)
             if not row:
